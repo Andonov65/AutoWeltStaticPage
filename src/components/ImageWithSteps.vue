47e42a3fcc20c109dc5642f781939fe51a4b8862
Marko Andonov <marko.20112000@gmail.com>
2022-07-11 11:30:46 +0200

    First commit

---
+++ src/components/ImageWithSteps.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <div>
+            <img class="ratio ratio-29x24" src="../assets/img/auto-welt-buying-steps-image.jpg">
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "ImageWithSteps"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="container">
+    <image-with-steps/>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import ImageWithSteps from "@/components/ImageWithSteps";
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {ImageWithSteps},
 }
 </script>
